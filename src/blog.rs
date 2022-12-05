75d67bef590c85b61d2fcc1d147afe100ffd2f16
Laurent Cimon <laurent@nilio.ca>
2022-12-05 09:57:21 -0500

    Add a new post page

--- src/blog.rs
+++ src/blog.rs
@@ -1,18 +1,21 @@
 use rocket::fairing::AdHoc;
+use rocket::form::{Form, FromForm};
 use rocket::response::content::RawHtml;
 use rocket::serde::{Serialize, Deserialize, json::Json};
 
 use rocket_sync_db_pools::rusqlite::params;
 use markdown_to_html::markdown;
 
-use super::{Db, Result};
+use super::{Db, Result, User};
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(FromForm, Clone, Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct Post {
-    id: u64,
+    id: Option<u64>,
+    #[field(name = "title")]
     title: String,
     submitted: Option<String>,
+    #[field(name = "text")]
     markdown: Option<String>
 }
 
@@ -21,7 +24,7 @@ async fn blog_posts(db: Db, username: String) -> Result<Json<Vec<Post>>> {
     let posts = db.run(move |conn| {
 	conn.prepare("SELECT id, title, submitted FROM posts WHERE username = ?1 ORDER BY submitted DESC")?
 	    .query_map(params![username], |row| Ok(Post {
-		id: row.get(0)?,
+		id: Some(row.get(0)?),
 		title: row.get(1)?,
 		submitted: Some(row.get(2)?),
 		markdown: None
@@ -37,7 +40,7 @@ async fn blog_post(db: Db, username: String, id: u64) -> Option<RawHtml<String>>
     let content: Post = db.run(move |conn| {
 	conn.query_row("SELECT title, submitted, markdown FROM posts WHERE username = ?1 AND id = ?2", params![username, id],
 		       |r| Ok(Post {
-			   id: id,
+			   id: Some(id),
 			   title: r.get(0)?,
 			   submitted: r.get(1)?,
 			   markdown: Some(r.get(2)?)
@@ -57,8 +60,26 @@ async fn new_blog_post(db: Db, username: String, post: Json<Post>) -> Option<Jso
     Some(post)
 }
 
+#[post("/new", data = "<post>")]
+async fn new_blog_post_form(db: Db, username: User, post: Form<Post>) -> Option<Json<Post>> {
+    let db_title = post.title.clone();
+    let db_markdown = post.markdown.clone();
+    let result = db.run(move |conn| {
+	let _ = conn.execute("INSERT INTO posts (username, title, markdown) VALUES (?1, ?2, ?3);", params![username.0, db_title, db_markdown]);
+	conn.query_row("SELECT id, title, submitted, markdown FROM posts WHERE username = ?1 AND title = ?2 AND markdown = ?3",
+		       params![username.0, db_title, db_markdown], |row| Ok(Post {
+			   id: Some(row.get(0)?),
+			   title: row.get(1)?,
+			   submitted: row.get(2)?,
+			   markdown: row.get(3)?
+		       }))
+    }).await.ok()?;
+
+    Some(Json(result))
+}
+
 pub fn stage() -> AdHoc {
     AdHoc::on_ignite("Blog Stage", |rocket| async {
-        rocket.mount("/blog", routes![blog_posts, blog_post, new_blog_post])
+        rocket.mount("/blog", routes![blog_posts, blog_post, new_blog_post, new_blog_post_form])
     })
 }
