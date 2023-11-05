eb9b87007588537e7dc9c7e91460e81cf1046b07
Laurent Cimon <laurent@nilio.ca>
2023-11-04 23:37:08 -0400

    Revert the last name changes and handle images in home

--- react/laurent/src/App.js
+++ react/laurent/src/App.js
@@ -21,7 +21,7 @@ function Header({loggedIn}) {
 	    <img src="/images/head.jpg" alt="My face" />
           </a>
           <div id="headerText">
-	    <h1>Laurent Cimon</h1>
+	    <h1>Laurent</h1>
 	    <h4>Full-Stack Developer</h4>
 	    <p>Rust | C++ | Lisp | Python | React</p>
           </div>
